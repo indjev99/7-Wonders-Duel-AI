fd3afacd0aed42b79e92527ac9174509598fefbe
Emil Indzhev <emil.indjev@gmail.com>
2023-02-06 18:36:03 +0100

    More progress on tracking changes

--- scripts/play_bga_game.py
+++ scripts/play_bga_game.py
@@ -8,6 +8,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 
+import sys
+
 def sanitize(name: str) -> str:
     return ''.join(filter(lambda x: x.isalpha() or x == ',' or x == ' ', name.lower()))
 
@@ -22,6 +24,8 @@ class BGAGame:
     def reset_state(self) -> None:
         self.names_to_ids = {}
         self.building_ids_to_names = {}
+        self.wonder_selection = True
+        self.state = {}
 
     def register_name_id(self, name: str, id: str) -> None:
         self.names_to_ids[name] = id
@@ -49,7 +53,7 @@ class BGAGame:
             else:
                 card_name = self.building_ids_to_names[card_bulding_id]
 
-            self.register_name(card_name, card_id)
+            self.register_name_id(card_name, card_id)
 
             container = card.find_element(By.XPATH, "../..")
             container_id = container.get_attribute('id')
@@ -64,6 +68,9 @@ class BGAGame:
             else:
                 built_cards.add(card_name)
 
+        if len(pyramid_poses_cards) > 0:
+            self.wonder_selection = False
+    
         return pyramid_poses_cards, guild_poses, built_cards, discarded_cards
 
     def find_tokens(self) -> tuple[set[str], set[str], set[str]]:
@@ -74,6 +81,9 @@ class BGAGame:
         for token in self.driver.find_elements(By.CLASS_NAME, 'progress_token'):
             token_name = sanitize(token.text)
             token_id = token.get_attribute('id')
+
+            if token_id == '' or token_id is None:
+                continue
 
             self.register_name_id(token_name, token_id)
 
@@ -89,7 +99,7 @@ class BGAGame:
 
         return game_tokens, box_tokens, built_tokens
 
-    def find_wonders(self, can_have_built: bool) -> tuple[set[str], set[str], set[str]]:
+    def find_wonders(self) -> tuple[set[str], set[str], set[str]]:
         revealed_wonders = set()
         selected_wonders = set()
         built_wonders = set()
@@ -98,10 +108,13 @@ class BGAGame:
             wonder_name = sanitize(wonder.text)
             wonder_id = wonder.get_attribute('id')
 
+            if wonder_id == '' or wonder_id is None:
+                continue
+
             self.register_name_id(wonder_name, wonder_id)
 
-            constructed = int(wonder.get_attribute('data-constructed'))
-            container = can_have_built and wonder.find_element(By.XPATH, "../../../..")
+            constructed = not self.wonder_selection and bool(int(wonder.get_attribute('data-constructed')))
+            container = wonder.find_element(By.XPATH, "../../../..")
 
             if container.get_attribute('id') == 'wonder_selection_block':
                 revealed_wonders.add(wonder_name)
@@ -120,7 +133,12 @@ class BGAGame:
         self.driver.get("https://boardgamearena.com/lobby")
 
     def select_by_id(self, id: str) -> None:
-        WebDriverWait(self.driver, 120).until(EC.element_to_be_clickable((By.ID, id))).click()
+        while True:
+            try:
+                WebDriverWait(self.driver, 120).until(EC.element_to_be_clickable((By.ID, id))).click()
+                return
+            except Exception:
+                pass
 
     def select_by_name(self, name: str) -> None:
         id = self.names_to_ids[name]
@@ -154,33 +172,153 @@ class BGAGame:
     def choose_go_second(self) -> None:
         self.select_by_id('buttonPlayerRight')
 
-    def start_game(self) -> None:
-        self.reset_state()
+    def start_bga_game(self) -> None:
         self.open_bga_page()
         self.create_game()
         self.open_table()
         self.accept_game()
 
+    def update_state(self, new_state: dict[set]) -> None:
+        if new_state == self.state:
+            return
+
+        def new_elems(name: str) -> set:
+            if name not in self.state:
+                return new_state[name]
+            return new_state[name].difference(self.state[name])
+
+        def old_elems(name: str) -> set:
+            if name not in self.state:
+                return set()
+            return self.state[name].difference(new_state[name])
+
+        actions = []
+
+        gone_pyramid_cards = set(map(lambda pos_card: pos_card[1], old_elems('pyramid_poses_cards')))
+        gone_built_cards = old_elems('built_cards')
+        gone_discarded_cards = old_elems('discarded_cards')
+        gone_game_tokens = old_elems('game_tokens')
+        gone_box_tokens = old_elems('box_tokens')
+        gone_revealed_wonders = old_elems('revealed_wonders')
+
+        for card in new_elems('discarded_cards'):
+            if card in gone_pyramid_cards:
+                actions.append(f'Discard card, {card}')
+                gone_pyramid_cards.remove(card)
+            elif card in gone_built_cards:
+                # TODO: color
+                actions.append(f'Destroy TODO, {card}')
+                gone_built_cards.remove(card)
+            else:
+                print(f'Unexplained discarded card, {card}', file=sys.stderr)
+                return
+
+        for card in new_elems('built_cards'):
+            if card in gone_pyramid_cards:
+                actions.append(f'Build card, {card}')
+                gone_pyramid_cards.remove(card)
+            elif card in gone_discarded_cards:
+                actions.append(f'Build discarded, {card}')
+                gone_discarded_cards.remove(card)
+            else:
+                print(f'Unexplained built card, {card}', file=sys.stderr)
+                return
+
+        for wonder in new_elems('built_wonders'):
+            if len(gone_pyramid_cards) > 0:
+                card = gone_pyramid_cards.pop()
+                actions.append(f'Build wonder, {card}, {wonder}')
+            else:
+                print(f'Unexplained built wonder, {wonder}', file=sys.stderr)
+                return
+    
+        for token in new_elems('built_tokens'):
+            if token in gone_game_tokens:
+                actions.append(f'Build token, {token}')
+                gone_game_tokens.remove(token)
+            elif token in gone_box_tokens:
+                actions.append(f'Build box token, {token}')
+                gone_game_tokens.remove(token)
+            elif len(self.state['box_tokens']) == 0 and len(new_state['box_tokens']) == 0:
+                # TODO: Reveal 2 random box tokens
+                actions.append(f'Reveal box token, {token}')
+                actions.append(f'Build box token, {token}')
+            else:
+                print(f'Unexplained built token, {token}', file=sys.stderr)
+                return
+
+        for wonder in new_elems('selected_wonders'):
+            if wonder in gone_revealed_wonders:
+                actions.append(f'Select wonder, {wonder}')
+                gone_revealed_wonders.remove(wonder)
+            else:
+                print(f'Unexplained selected wonder, {card}', file=sys.stderr)
+                return
+
+        if len(gone_pyramid_cards) > 0:
+            print(f'Unexplained gone pyramid cards, {gone_pyramid_cards}', file=sys.stderr)
+            return
+
+        if len(gone_built_cards) > 0:
+            print(f'Unexplained gone built cards, {gone_built_cards}', file=sys.stderr)
+            return
+
+        if len(gone_discarded_cards) > 0:
+            print(f'Unexplained gone discarded cards, {gone_discarded_cards}', file=sys.stderr)
+            return
+
+        if len(gone_game_tokens) > 0:
+            print(f'Unexplained gone tokens, {gone_game_tokens}', file=sys.stderr)
+            return
+
+        if len(gone_revealed_wonders) > 0:
+            print(f'Unexplained gone revealed wonders, {gone_revealed_wonders}', file=sys.stderr)
+            return
+
+        for game_token in new_elems('game_tokens'):
+            actions.append(f'Reveal token, {game_token}')
+
+        for box_token in new_elems('box_tokens'):
+            actions.append(f'Reveal box token, {box_token}')
+
+        for pos in new_elems('guild_poses'):
+            actions.append(f'Reveal guild, {pos}')
+
+        for pos, card in sorted(list(new_elems('pyramid_poses_cards'))):
+            actions.append(f'Reveal card, {card}, {pos}')
+
+        for wonder in new_elems('revealed_wonders'):
+            actions.append(f'Reveal wonder, {wonder}')
+
+        for action in actions:
+            print(action)
+
+        self.state = new_state
+
     def play_game(self) -> None:
-        self.start_game()
-
-        can_have_built = False
-
-        old_everything = []
+        self.start_bga_game()
+        self.reset_state()
 
         while True:
             try:
                 pyramid_poses_cards, guild_poses, built_cards, discarded_cards = self.find_cards()
                 game_tokens, box_tokens, built_tokens = self.find_tokens()
-                can_have_built |= len(pyramid_poses_cards) > 0
-                revealed_wonders, selected_wonders, built_wonders = self.find_wonders(can_have_built)
+                revealed_wonders, selected_wonders, built_wonders = self.find_wonders()
 
-                everything = [pyramid_poses_cards, guild_poses, built_cards, discarded_cards, game_tokens, box_tokens, built_tokens, revealed_wonders, selected_wonders, built_wonders]
+                new_state = {
+                    'pyramid_poses_cards' : pyramid_poses_cards,
+                    'guild_poses' : guild_poses,
+                    'built_cards' : built_cards,
+                    'discarded_cards' : discarded_cards,
+                    'game_tokens' : game_tokens,
+                    'box_tokens' : box_tokens,
+                    'built_tokens' : built_tokens,
+                    'revealed_wonders' : revealed_wonders,
+                    'selected_wonders' : selected_wonders,
+                    'built_wonders' : built_wonders
+                }
 
-                if everything != old_everything:
-                    old_everything = everything
-                    print(everything)
-                    print()
+                self.update_state(new_state)
 
             except Exception:
                 pass
